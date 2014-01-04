25ded33ae849d7a9a354d13c38d859def80c2a96
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2014-01-04 22:12:07 +0000

    Split into core and script. Add project operator.

--- MiniKanren/CoreMiniKanren.fs
+++ MiniKanren/CoreMiniKanren.fs
@@ -232,99 +232,15 @@ let run n f =
 
 let newVar() = Var (new Var())
 
-//tests
-let bla = run 5 (fun myvar -> let x = newVar() in (equiv x (Atom 3)) .& (equiv myvar x))
 
-let g = run 5 (fun x -> equiv x (Atom 1))
-
-let g2 = run 5 (fun q -> 
-            let (x,y,z) = newVar(),newVar(),newVar()
-            equiv (Term.FromSeq [x; y; z; x]) q
-            .| equiv (Term.FromSeq [z; y; x; z]) q)
-
-let g3 = run 1(fun q-> 
-            let x,y = newVar(),newVar()
-            equiv y q
-            .& equiv (Atom 3) y)
+//impure operators
+let project (v:Term) (f:obj -> Goal<_>) : Goal<_> =
+    fun s -> 
+        printfn "s=%A" s
+        //assume atom here..otherwise fail
+        let (Atom x) = walkMany v s
+        printfn "x=%A" x
+        f x s
 
 
-let g4 = run 5 (fun q -> 
-            let x,y,z = newVar(),newVar(),newVar()
-            equiv (List (x, y)) q
-            .| equiv (List (y, y)) q)
 
-
-let infinite = run 9 (fun q ->  
-                let rec loop() =
-                    conde <|
-                        seq { yield equiv (Atom false) q,[]
-                              yield equiv (Atom true) q, []
-                              yield loop(),[] }
-                loop())
-
-//anyo tries g an unbounded number of times
-let rec anyo g =
-    conde <| seq { yield g,[]; yield anyo g,[] }
-
-let anyTest = run 5 (fun q -> anyo (equiv (Atom false) q)
-                              .| equiv (Atom true) q)
-
-let anyTest2 =  
-    run 5 (fun q -> 
-        anyo (equiv (Atom 1) q
-              .| equiv (Atom 2) q
-              .| equiv (Atom 3) q))
-
-let alwayso = anyo (equiv Nil Nil)
-
-let alwaysoTest =
-    run 15 (fun x ->
-        (equiv (Atom true) x .| equiv (Atom false) x)
-        .& alwayso
-        .& equiv (Atom false) x)
-
-let alwaysoTest2 =
-    run 3 (fun q ->
-        let nevero = anyo (equiv (Atom true) (Atom false))
-        equiv (Atom 1) q
-        .| nevero
-        .| (equiv (Atom 2) q
-            .| nevero
-            .| equiv (Atom 3) q)) 
-
-
-let appendo l s out = 
-    let appendoF appendo (l:Term,s:Term,out:Term) =
-        equiv Nil l => equiv s out
-        .| (let a,d = newVar(),newVar()
-            equiv (List (a,d)) l
-            .& (let res = newVar() 
-                appendo (d,s,res) //causes stackoverflow without fix
-                .& equiv (List (a,res)) out))
-    fix appendoF (l,s,out)
-
-let appendoTest =
-    run 1 (fun q -> appendo q (List (Atom 5, List(Atom 4,Nil))) (List (Atom 3, List (Atom 5, List(Atom 4, Nil)))))
-
-let appendoTest2 =
-    run 3 (fun q -> 
-        let l,s = newVar(),newVar()
-        appendo l s (List (Atom 1, List(Atom 2,Nil)))
-        .& equiv (List (l,s)) q)
-
-[<EntryPoint>]
-let main args =
-    printfn "%O" bla
-    printfn "%O" g
-    printfn "%O" g2
-    printfn "%O" g3
-    printfn "%O" g4
-    printfn "%O" infinite
-    printfn "%O" anyTest
-    printfn "%O" anyTest2
-    printfn "%O" alwaysoTest
-    printfn "%O" alwaysoTest2
-    printfn "%A" appendoTest
-    printfn "%A" appendoTest2
-    //Console.ReadKey() |> ignore
-    0
